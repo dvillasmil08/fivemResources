9946e5b434bb76b97d439d0019ca88d4c05fbc20
dvillasmil08 <dvillasmil08@gmail.com>
2024-01-29 11:29:00 -0500

    created simple notification

--- [main]/ch_gui/client/client.lua
+++ [main]/ch_gui/client/client.lua
@@ -1,8 +1,22 @@
 -- Notification
 
-function showNotification()
+function showNotification(message, color, flash, saveToBrief)
+    BeginTextCommandThefeedPost("STRING")
+    AddTextComponentSubstringPlayerName(message)
+    ThefeedNextPostBackgroundColor(color)
+    EndTextCommandThefeedPostTicker(flash, saveToBrief)
     
 end
+
+RegisterCommand('testNotification', function (_,_, rawCommand)
+    showNotification(
+        rawCommand,
+        130,
+        true,
+        false
+    
+    )
+end)
 
 
 -- Advanced Notifications
